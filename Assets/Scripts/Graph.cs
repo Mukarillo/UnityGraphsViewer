2ecb67c1c53168d4a4ad948726abe79166ec11bd
Murillo Lopes <mukarillo@Murillos-MacBook-Pro.local>
2018-01-20 00:50:13 -0200

    first commit

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MathParserTK;
+
+public class Graph : MonoBehaviour {
+
+    public Transform pointPrefab;
+    [Range(10, 100)]
+    public int resolution = 10;
+    public bool animate = true;
+
+    private Transform[] mPoints = new Transform[]{};
+    private MathParser mMathParser = new MathParser('.');
+    private string mStringFunction;
+    private bool mShowGraph = false;
+
+    private void Awake(){
+        float step = 2f / resolution;
+        Vector3 scale = Vector3.one * step;
+        scale.z = 3f;
+        Vector3 position = Vector3.zero;
+
+        mPoints = new Transform[resolution];
+        for (int i = 0; i < resolution; i++)
+        {
+            Transform point = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) * step - 1f;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform, false);
+
+            mPoints[i] = point;
+        }
+
+        SetGraphVisibility(mShowGraph);
+    }
+
+    private void Update(){
+        if (!mShowGraph) return;
+
+        for (int i = 0; i < mPoints.Length; i++)
+        {
+            Transform point = mPoints[i];
+            Vector3 position = point.localPosition;
+
+            var fClone = mStringFunction;
+            fClone = fClone.Replace("x", "("+position.x.ToString()+")").Replace("t", (animate ? Time.time.ToString() : "0"));
+            double functionResult = 0;
+
+            if(!mMathParser.TryParse(fClone, out functionResult, true)){
+                UIManager.instance.SetMessageInInputField("Invalid Function");
+                continue;
+            }
+
+            position.y = (float)functionResult;
+            point.localPosition = position;
+        }
+    }
+
+    public void SetStringFunction(string function){
+        mStringFunction = function;
+        mShowGraph = true;
+        SetGraphVisibility(mShowGraph);
+    }
+
+    private void SetGraphVisibility(bool show){
+        for (int i = 0; i < mPoints.Length; i++)
+        {
+            mPoints[i].gameObject.SetActive(show);
+        }
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    public static UIManager instance;
+
+    public Graph graph;
+    public InputField inputField;
+
+
+    private void Awake(){
+        instance = this;
+    }
+
+    public void SetFunction(){
+        graph.SetStringFunction(inputField.text);
+    }
+
+    public void ToggleAnimation(bool value){
+        graph.animate = value;
+    }
+
+    public void SetMessageInInputField(string message){
+        inputField.placeholder.GetComponent<Text>().text = message;
+    }
+}
